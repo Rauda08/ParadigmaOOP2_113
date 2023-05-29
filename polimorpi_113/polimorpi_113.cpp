52a1c951f7a688a0912289e90c1e68ea0d2d5be5
rauda08 <raudajnnh08@gmail.com>
2023-05-29 11:30:14 +0700

    mengembalikan nilai 0

--- polimorpi_113/polimorpi_113.cpp
+++ polimorpi_113/polimorpi_113.cpp
@@ -8,3 +8,32 @@ public:
 	//	cout<<"Pesan dari seseorang'<<endl;
 	// }
 };
+
+class joko : public seseorang {
+public:
+	void pesan() {
+		cout << "pesan dari joko" << endl;
+	}
+};
+
+class lia :public seseorang {
+public:
+	void pesan() {
+		cout << "pesan dari lia" << endl;
+	}
+
+};
+
+int main() {
+	seseorang* obyek;
+	joko a;
+	lia b;
+
+	obyek = &a;
+	obyek->pesan();
+	obyek = &b;
+	obyek->pesan();
+	//a.seseorang:: pesan()
+
+	return 0;
+}
