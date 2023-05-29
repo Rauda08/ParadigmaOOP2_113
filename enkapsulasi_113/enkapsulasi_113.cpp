8dce0b4cf934f091401d46faeb42b774f7e0ab68
rauda08 <raudajnnh08@gmail.com>
2023-05-29 13:12:58 +0700

    membuat deklarasi remotelampu

--- enkapsulasi_113/enkapsulasi_113.cpp
+++ enkapsulasi_113/enkapsulasi_113.cpp
@@ -5,8 +5,18 @@ class remoteLampu {
 private:
     string saklarNo[10];
 public:
-    void setSaklarNo(int i, string value){}
+    void setSaklarNo(int i, string value){
+        saklarNo[i] = value;
+    }
+    string getSaklarNo(int i) {
+        return saklarNo[i];
+    }
 };
+
+int main(){
+    remotelampu lampuRumah;
+
+}
 
 int main()
 {
